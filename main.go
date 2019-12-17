7bd85260a9f882c1c8db789e71d9cd1352b04d5c
Caleb Bron <caleb@rancher.com>
2019-12-17 16:50:38 -0700

    pr11

--- main.go
+++ main.go
@@ -18,7 +18,7 @@ func home(w http.ResponseWriter, r *http.Request) {
 	// Arbitrary sleep so that we can demonstrate autoscaler
 	log.Println("requests received")
 	time.Sleep(101 * time.Millisecond)
-	fmt.Fprintln(w, "pr10")
+	fmt.Fprintln(w, "pr11")
 }
 
 func main() {
